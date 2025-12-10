58e9d20520154dd3b0a6f50bc02b3a72c981e8f7
weiweiying <weiying.wei@leadbank.com.cn>
2025-12-10 17:38:55 +0800

    配置文件

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   output: "standalone",
-  experimental: {
-    outputFileTracingIncludes: {
-      "/": ["./public/**/*"],
-    },
-  },
 };
 
 module.exports = nextConfig;
