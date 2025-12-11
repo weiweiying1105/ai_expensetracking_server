391fdaacc6d78f634255f252b0709b108c48fed0
weiweiying <weiying.wei@leadbank.com.cn>
2025-12-11 13:35:33 +0800

    // 限制webpack扫描范围，避免访问系统目录

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // 限制webpack扫描范围，避免访问系统目录
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
