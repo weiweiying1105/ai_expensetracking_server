693a20a219b9c596740823216ca3d8eb76a28465
weiweiying <weiying.wei@leadbank.com.cn>
2025-12-23 14:15:49 +0800

    remove lint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  eslint: {
+    // 禁用ESLint检查
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
